9acee050a443f676dd5042f7d4195da704ebf65a
PradeepRamakrishnan <pradeepramakrishnan9001@gmail.com>
2020-10-24 15:16:25 +0530

    Task completed

--- src/test/java/org/task/Gitsetup.java
+++ src/test/java/org/task/Gitsetup.java
@@ -2,6 +2,8 @@ package org.task;
 
 public class Gitsetup {
 	public static void main(String[] args) {
-System.out.println("Branch Pradeep is pulled");
+		System.out.println("Branch Pradeep is pulled");
+		System.out.println("Branch is updated");
+		System.out.println("111111");
 	}
 }
